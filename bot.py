54aaaf99c8724382bf1b552722f6f5ad247b3d8c
cheesiscool <cheesiscool@hotmail.com>
2015-12-31 22:54:31 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -176,7 +176,7 @@ while True:
                                                     send("G0H17UA5S", "Name not found! Please try again!")
                                             #Handle idea deletion
                                             elif (message.lower()[:9] == "!delidea ") and (channelstatus[0]['channel'] == "G0H17UA5S"):
-                                                (m, num) = message.split(" ",maxsplit = 1)
+                                                (m, num) = (message.split(maxsplit = 1)).strip()
                                                 try:
                                                     #Makes sure "1" points to d[userID][0]
                                                     num = int(num) - 1
