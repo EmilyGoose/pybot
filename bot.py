56f49b8271435b4621ea6f2612830f0eb4838011
ncarr <nicholas.carr@rogers.com>
2016-04-09 23:56:01 -0400

    New syntax
    Also more syntax-agnostic code, in case we want to change it later

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 #github.com/MishaLarionov/pybot/tree/discord-unstable
 
 #TODO:
-#Rework command system
 #Support multiple servers from one bot instance
 #Add todo command
 
@@ -47,7 +46,7 @@ def writedict(d):
     f.close()
     return()
 
-def newidea(name, text):
+def newidea(text, user, channel):
     debug = client.get_channel(cfg.DEBUGCH)
     try:
         #Grab the dictionary from the file
@@ -55,23 +54,21 @@ def newidea(name, text):
         d = readfile()
         try:
             #Add the idea to the user's list of ideas
-            d[name].append(text)
+            d[user.id].append(text)
         except:
             #Create the user in the dictionary if they don't exist
-            d[name] = [text]
+            d[user.id] = [text]
         writedict(d)
         if readfile() == {}:
             client.send_message(debug, "Something wiped the file. Restoring to previous version...")
             writedict(dprotect)
-        return()       
     except Exception as e:
-        client.send_message(message.channel, "Sorry, I couldn't add your idea. Please try again!")
+        client.send_message(channel, "Sorry, I couldn't add your idea. Please try again!")
         writedict(dprotect)
     else:
-        client.send_message(message.channel, "{}'s idea has been added.".format(message.author.mention()))
+        client.send_message(channel, "{}'s idea has been added.".format(user.mention()))
 
 def getideas(name, channel):
-    #message is not defined FIX
     userNames = []
     userIDs = []
     for server in client.servers:
@@ -101,28 +98,64 @@ def getideas(name, channel):
     else:
         client.send_message(channel, "Name not found! Please try again!")
 
+def delidea(num, author, channel):
+    try:
+        #Makes sure "1" points to d[userID][0]
+        num = int(num) - 1
+        if num < 0:
+            num = num + 1
+        #Grab the dictionary from the text file
+        d = readfile()
+        #Make sure the number is not greater than the amount of elements
+        if (num + 1) > len(d[author]):
+            client.send_message(channel, "The number you entered is too large. Please try again.")
+        else:
+            #Get rid of the element
+            e = d[author].pop(num)
+            #Rebuild the dictionary
+            writedict(d)
+            client.send_message(channel, "Idea `" + e.replace("`", "'") + "` deleted.")
+    except:
+        client.send_message(channel, "Invalid number. Please try again.")
+
+def setreminder():
+    event = message.content.split(" ", maxsplit = 1)[1]
+    (name, datetime) = event.split("@", maxsplit = 1)
+    name = name.strip()
+    dtime = dateparser.parse(datetime)
+    s = sched.scheduler()
+    s.enterabs(dtime.timestamp(), 1, (remind, message.channel), name)
+    s.run()
+
 def processcommand(rawstring, channel, user):
     #This function doesn't do anything yet but it will eventually
-    (cmd, message) = rawstring.split(" ", maxsplit = 1)
-    if cmd == "hello":
-        client.send_message(channel, 'Hello {}!'.format(user.mention()))
-    elif cmd == "idea":
-        newidea(user.id, message)
-    elif cmd == "getideas":
-        getideas(message, channel)
-        pass
-    elif cmd == "delidea":
-        #Code goes here someday
-        pass
-    elif cmd == "remind":
-        #Code goes here someday
-        pass
-    elif cmd == "help":
-        #Code goes here someday
-        pass
-    elif cmd == "die":
-        #Secure killswitch
-        pass
+    if " " in rawstring:
+        (cmd, message) = rawstring.split(" ", maxsplit = 1)
+        if cmd == "hello":
+            client.send_message(channel, 'Hello {}!'.format(user.mention()))
+        elif cmd == "idea" or cmd == "idea:":
+            newidea(message, user, channel)
+        elif cmd == "getideas":
+            getideas(message, channel)
+        elif cmd == "delidea":
+            delidea(message, user.id, channel)
+        elif cmd == "remind":
+            #Code goes here someday
+            pass
+        elif cmd == "help":
+            client.send_message(channel, "PYBOT V5 HELP\n`@pybot help` shows this page\n`@pybot idea: <text>` Records a suggestion in your name. You can see it with @pybot getideas\n`@pybot getideas <username>` lists ideas from user. *username* can be omitted to get your own ideas.\n`@pybot delidea <n>` deletes the idea with the number *n*\n`@pybot machineinfo` Returns server name and operating system")
+    else:
+        if rawstring == "hello":
+            client.send_message(channel, 'Hello {}!'.format(user.mention()))
+        if rawstring == "die":
+            #This should be more secure eventually
+            sys.exit()
+        elif rawstring == "machineinfo":
+            client.send_message(channel, platform.node() + " " + platform.platform())
+        elif rawstring == "help":
+            client.send_message(channel, "PYBOT V5 HELP\n`@pybot help` shows this page\n`@pybot idea: <text>` Records a suggestion in your name. You can see it with @pybot getideas\n`@pybot getideas <username>` lists ideas from user. *username* can be omitted to get your own ideas.\n`@pybot delidea <n>` deletes the idea with the number *n*\n`@pybot machineinfo` Returns server name and operating system")
+        elif rawstring == "getideas":
+            getideas(user.name, channel)
 
 def remind(name, channel):
     client.send_message(channel, "You asked me to remind you to" + name)
@@ -134,53 +167,11 @@ def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
+    if message.content.startswith("!") and len(message.content) > 1:
+        processcommand(message.content[1:], message.channel, message.author)
+    if message.content.startswith("@pybot") and len(message.content) > 7:
+        processcommand(message.content[7:], message.channel, message.author)
 
-    if message.content.startswith('!hello'):
-        client.send_message(message.channel, 'Hello {}!'.format(message.author.mention()))
-    #Handle new ideas
-    if message.content.startswith('!idea:'):
-        newidea(message.author.id, message.content.split(": ", maxsplit = 1)[1])
-    #Handle !getideas calls
-    elif message.content.startswith("!getideas "):
-        getideas(message.content.split(" ", maxsplit = 1)[1], message.channel)
-    #Handle idea deletion
-    elif message.content.startswith("!delidea"):
-        (m, num) = message.content.split(" ", maxsplit = 2)
-        try:
-            #Makes sure "1" points to d[userID][0]
-            num = int(num) - 1
-            if num < 0:
-                num = num + 1
-            #Grab the dictionary from the text file
-            d = readfile()
-            #Make sure the number is not greater than the amount of elements
-            if (num + 1) > len(d[message.author.id]):
-                client.send_message(message.channel, "The number you entered is too large. Please try again.")
-            else:
-                #Get rid of the element
-                e = d[message.author.id].pop(num)
-                #Rebuild the dictionary
-                writedict(d)
-                client.send_message(message.channel, "Idea `" + e.replace("`", "'") + "` deleted.")
-        except:
-            client.send_message(message.channel, "Invalid number. Please try again.")
-    #Steal server info
-    elif message.content.startswith("!machineinfo"):  
-        client.send_message(message.channel, platform.node() + " " + platform.platform())
-    #Reminders (currently disabled)
-    elif message.content.startswith("!remind") and False:
-        event = message.content.split(" ", maxsplit = 1)[1]
-        (name, datetime) = event.split("@", maxsplit = 1)
-        name = name.strip()
-        dtime = dateparser.parse(datetime)
-        s = sched.scheduler()
-        s.enterabs(dtime.timestamp(), 1, (remind, message.channel), name)
-        s.run()
-    elif message.content.startswith("!killswitch"):
-        #This should be more secure eventually
-        sys.exit()
-    elif message.content.startswith("!help"):
-        client.send_message(message.channel, "PYBOT V5 HELP\n`!help` shows this page\n`!idea: <text>` Records a suggestion in your name. You can see it with !getideas\n`!getideas <username>` lists ideas from user\n`!delidea <n>` deletes the idea with the number n\n`!machineinfo` Returns server name and operating system")
 @client.event
 def on_ready():
     debug = client.get_channel(cfg.DEBUGCH)
